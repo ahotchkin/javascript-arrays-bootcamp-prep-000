56b7d87dbf303c6847d17774344e7df759c9baea
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-28 02:10:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,5 +6,6 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return ['foo', ...array]
+  ['foo', ...array];
+  return array
 }
