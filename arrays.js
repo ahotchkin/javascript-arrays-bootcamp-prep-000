1e2a5ea625b0d87dd7e44fabc4e394b46ae8bb3f
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-28 02:07:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  const newArray = [1, ...array]
+  const newArray = ['foo', ...array]
 }
